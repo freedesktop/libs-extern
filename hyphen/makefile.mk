ebe31ad419d04f9e3e2fa4001fb12c494e67ba00
Jens-Heiner Rechtien <hr@openoffice.org>
2010-02-12 15:01:35 +0100

    changefileheader2: #i109125#: change source file copyright notice from Sun Microsystems to Oracle; remove CVS style keywords (RCSfile, Revision)

--- hyphen/makefile.mk
+++ hyphen/makefile.mk
@@ -2,13 +2,9 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 # 
-# Copyright 2008 by Sun Microsystems, Inc.
+# Copyright 2000, 2010 Oracle and/or its affiliates.
 #
 # OpenOffice.org - a multi-platform office productivity suite
-#
-# $RCSfile: makefile.mk,v $
-#
-# $Revision: 1.8.2.3 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -75,7 +71,7 @@ CONFIGURE_FLAGS+=CFLAGS='$(LCL_CONFIGURE_CFLAGS)'
 BUILD_ACTION=make hyph_en_US.dic
 .ELIF "$(SYSTEM_HYPH)" == "YES" && "$(WITH_MYSPELL_DICTS)" != "YES"
 @all:
-    echo "Nothing to do here."
+	echo "Nothing to do here."
 .ELSE
 BUILD_ACTION=make
 OUT2INC += hyphen.h 
